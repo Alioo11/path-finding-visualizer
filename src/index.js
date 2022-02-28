de0e61378181c7abbdc306f9334d2684ed4c151c
ali <110mors110@gmail.com>
2022-02-28 20:30:59 +0330

    added some interaction change

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import { dijkestra, dijkestra_realTime } from './algorithms/dijkestra_heap_based
 import { normalDijkestras, normalDijkestras_realTime } from './algorithms/dijkestra_normal.js'
 import { backTracker } from './algorithms/back-tracker.js'
 import { dimentions as DM } from './utils/config.js'
-import { a_start, a_start2, a_start_realTime} from './algorithms/a_start.js'
-import { greedy, greedy_realTime} from './algorithms/greedy.js'
+import { a_start, a_start2, a_start_realTime } from './algorithms/a_start.js'
+import { greedy, greedy_realTime } from './algorithms/greedy.js'
 import { visPath } from './helpers/visPath.js'
 import { onWaiting } from './helpers/wait.js'
-import { basicRandom} from './algorithms/basic_randomized.js'
+import { basicRandom } from './algorithms/basic_randomized.js'
 import { basic_random } from './algorithms/basic-random.js'
-import { randomized_kruskal2} from './algorithms/randomized_kruskal.js'
-import {first_depth} from './algorithms/firstDepth.js'
+import { randomized_kruskal2 } from './algorithms/randomized_kruskal.js'
+import { first_depth } from './algorithms/firstDepth.js'
 import "./../styles.css"
 
 document.getElementById("algo_dijkestra").addEventListener('click', (e) => {
@@ -23,22 +23,26 @@ document.getElementById("algo_A_start").addEventListener('click', (e) => {
 document.getElementById("algo_dijkestra_heap_based").addEventListener('click', (e) => {
   localStorage.setItem("algorithm", "dijkestra_heap_based")
 })
-document.getElementById('maze_recursive_backtracking').addEventListener('click',()=>{
-  localStorage.setItem('maze',"recursive_backtracking" )
+document.getElementById('maze_recursive_backtracking').addEventListener('click', () => {
+  localStorage.setItem('maze', "recursive_backtracking")
 })
-document.getElementById('maze_basic_random').addEventListener('click',()=>{
-  localStorage.setItem('maze',"basic_random")
+document.getElementById('maze_basic_random').addEventListener('click', () => {
+  localStorage.setItem('maze', "basic_random")
 })
-document.getElementById("maze_randomized_kruskal").addEventListener('click',()=>{
-  localStorage.setItem('maze',"randomized_kruskal")
+document.getElementById("maze_randomized_kruskal").addEventListener('click', () => {
+  localStorage.setItem('maze', "randomized_kruskal")
 })
 
 document.getElementById("algo_greedy").addEventListener('click', (e) => {
   localStorage.setItem("algorithm", "greedy")
 })
-document.querySelector("#clear_maze").addEventListener('click', () => cleanBoard())
+
+document.querySelector("#clear_maze").addEventListener('click', () => cleanBoard("all"))
+document.querySelector("#clear_path").addEventListener('click', () => cleanBoard("path"))
+document.querySelector("#clear_walls").addEventListener('click', () => cleanBoard("walls"))
+
 const detailMood = document.getElementById("detail-mode")
-document.getElementById("detail-mode").addEventListener('click' ,()=>{
+document.getElementById("detail-mode").addEventListener('click', () => {
   handleCheckbox()
 })
 
@@ -67,16 +71,16 @@ let hodledElement = null;
 let tempNode = null
 
 document.addEventListener('mousedown', (e) => {
-  if(progressing) return
-  if (e.target.className == "entry" || e.target.className == "target"){
+  if (progressing) return
+  if (e.target.className == "entry" || e.target.className == "target") {
     hodledElement = e.target.className
     hodingClick = true;
   }
   //TODO                                                                       don't forget to change cursor icon 
-} )
+})
 
 
-document.addEventListener('mouseup', (e) =>{ 
+document.addEventListener('mouseup', (e) => {
   if (progressing) return
   hodingClick = false
   hodledElement = null
@@ -90,15 +94,15 @@ const entry = "entry";
 const weight = 'weight'
 const cell = "cell"
 
-const handleCheckbox = ()=>{
+const handleCheckbox = () => {
   localStorage.setItem('detail-mode', detailMood.checked)
 }
 
-export const waitTillUserClick = ()=>{
-  return new Promise((resolve , reject)=>{
-    document.addEventListener('keydown' , (e)=>{
-      if(e.key === 'f' || e.key == 'F'){
-      return resolve()  
+export const waitTillUserClick = () => {
+  return new Promise((resolve, reject) => {
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'f' || e.key == 'F') {
+        return resolve()
       }
     })
   })
@@ -128,18 +132,18 @@ function init() {
 
   step = 3
 
-  const midBoard = Math.floor(HEIGHT/2 ) -1
+  const midBoard = Math.floor(HEIGHT / 2) - 1
 
-  const targetIndex = WIDTH * midBoard + Math.floor(0.8*WIDTH)
-  const endtryIndex = WIDTH * midBoard + Math.floor(0.2*WIDTH)
+  const targetIndex = WIDTH * midBoard + Math.floor(0.8 * WIDTH)
+  const endtryIndex = WIDTH * midBoard + Math.floor(0.2 * WIDTH)
 
   console.log(targetIndex)
 
   targetNode = cells[targetIndex]
-  draw(targetNode , target)
+  draw(targetNode, target)
 
   entryNode = cells[endtryIndex]
-  draw(entryNode , entry)
+  draw(entryNode, entry)
 
 }
 init()
@@ -159,21 +163,35 @@ startBtn.addEventListener('click', () => {
 
 
 
-const cleanBoard = async () => {
-  if(progressing) return
+const cleanBoard = async (type) => {
+  if (progressing) return
   step = 1
-  for(let i =0 ; i<cells.length ; i++){
-    i % WIDTH == 0 &&  await onWaiting(40)
-    forceDraw(cells[i] , cell)
-    forceDraw(cells[cells.length - i -1], cell)
-    writeIn(cells[i],'')
-    writeIn(cells[cells.length - i - 1], '')
+  for (let i = 0; i < cells.length; i++) {
+    i % WIDTH == 0 && await onWaiting(20)
+    if (type == "all") {
+      forceDraw(cells[i], cell)
+      forceDraw(cells[cells.length - i - 1], cell)
+      writeIn(cells[i], '')
+      writeIn(cells[cells.length - i - 1], '')
+    }
+    else if (type == "walls") {
+      step = 3
+      if (cells[i].className === "wall") {
+        forceDraw(cells[i], cell)
+      }
+    }
+    else if (type == "path") {
+      step = 3
+      if (cells[i].className === "scaning" || cells[i].className === "path") {
+        forceDraw(cells[i], cell)
+      }
+    }
   }
 }
 
 
 const handleSenario = (NodeCell) => {
-  if(progressing) return
+  if (progressing) return
   switch (step) {
     case 0: {
       backTracker(cells[WIDTH + 1], true)
@@ -181,7 +199,7 @@ const handleSenario = (NodeCell) => {
       break;
     }
     case 1: {
-      draw(NodeCell, target )
+      draw(NodeCell, target)
       targetNode = NodeCell;
       step += 1
       break;
@@ -196,44 +214,44 @@ const handleSenario = (NodeCell) => {
 }
 
 const startVis = async (fast_forward) => {
-  if(progressing) return
+  if (progressing) return
   if (step === 3 && entryNode) {
     const algorithm = localStorage.getItem("algorithm") ? localStorage.getItem("algorithm") : null
     console.time()
     progressing = true
     switch (algorithm) {
       case "dijkestra_heap_based": {
-        if(fast_forward){
-        dijkestra_realTime(new Node(entryNode, null, entry, 0))
-        break;
+        if (fast_forward) {
+          dijkestra_realTime(new Node(entryNode, null, entry, 0))
+          break;
         }
         await dijkestra(new Node(entryNode, null, entry, 0))
         break;
       }
       case "dijkestra": {
-        if (fast_forward){
+        if (fast_forward) {
           normalDijkestras_realTime(new Node(entryNode, null, entry, 0))
           break;
-        }else{
+        } else {
           await normalDijkestras(new Node(entryNode, null, entry, 0))
-        break;
+          break;
         }
       }
       case "a_start": {
-        if(fast_forward){
+        if (fast_forward) {
           a_start_realTime(new Node(entryNode, null, entry, 0), targetNode)
           break;
-        }else{
+        } else {
           await a_start2(new Node(entryNode, null, entry, 0), targetNode)
-        break;
+          break;
         }
 
       }
       case "greedy": {
-        if(fast_forward){
+        if (fast_forward) {
           greedy_realTime(new Node(entryNode, null, entry, 0), targetNode)
           break;
-        }else{
+        } else {
           greedy(new Node(entryNode, null, entry, 0), targetNode)
           break;
         }
@@ -248,14 +266,14 @@ const startVis = async (fast_forward) => {
 }
 
 const selectAndCreateMaze = async () => {
-  if(progressing) return
+  if (progressing) return
   progressing = true
   step = 1;
   entryNode && draw(entryNode, cell)
   targetNode && forceDraw(targetNode, cell)
   const mazeAlgo = localStorage.getItem("maze")
-  switch (mazeAlgo){
-    case "recursive_backtracking" :{
+  switch (mazeAlgo) {
+    case "recursive_backtracking": {
       await backTracker(cells[WIDTH + 1], true)
       break;
     }
@@ -263,21 +281,21 @@ const selectAndCreateMaze = async () => {
       await backTracker(cells[WIDTH + 1], true)
       break;
     }
-    case "basic_random" : {
+    case "basic_random": {
       await basicRandom()
       break
     }
-    case "randomized_kruskal":{
+    case "randomized_kruskal": {
       await randomized_kruskal2()
       break
     }
-    default :{
+    default: {
       await randomized_kruskal2()
       break
     }
   }
-    console.log('here we finished the algorithms')
-    console.log('hahaha')
+  console.log('here we finished the algorithms')
+  console.log('hahaha')
   progressing = false
   //
   //basicRandom()
@@ -311,23 +329,23 @@ cells.forEach((cellNode) => {
       writeIn(e.target, 3)
     }
     if (isWActive) {
-      draw(e.target, wall )
+      draw(e.target, wall)
     }
     if (isEActive) {
       draw(e.target, cell)
     }
-    if(hodingClick){
+    if (hodingClick) {
       tempNode = e.target.className
-      if (hodledElement == 'entry' && e.target.className !== 'target' || hodledElement == 'target' && e.target.className !== 'entry'){
-        forceDraw(e.target , hodledElement)
+      if (hodledElement == 'entry' && e.target.className !== 'target' || hodledElement == 'target' && e.target.className !== 'entry') {
+        forceDraw(e.target, hodledElement)
         if (hodledElement == 'entry') {
           entryNode = e.target
         } else if (hodledElement == 'target') {
           targetNode = e.target
         }
         // console.log(step);
-        if(step > 1){
-         startVis(true) 
+        if (step > 1) {
+          startVis(true)
         }
       }
     }
@@ -336,9 +354,9 @@ cells.forEach((cellNode) => {
 
 cells.forEach((cellNode) => {
   cellNode.addEventListener("mouseout", (e) => {
-      if (hodingClick) {
-        forceDraw(e.target, tempNode ? tempNode : cell)
-      } 
-    })
+    if (hodingClick) {
+      forceDraw(e.target, tempNode ? tempNode : cell)
+    }
+  })
 
 })
