0827a8acdc1722edcb43704e253e612289b50d0b
ali <110mors110@gmail.com>
2022-03-07 18:42:54 +0330

    fix some webpack style issue

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,25 @@ const path = require("path")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const merge = require('webpack-merge')
 const common = require("./webpack.common")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = merge.merge(common , {
+    plugins : [
+        new HtmlWebpackPlugin({template:"./index.html",}),
+        new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+],
     mode: "production",
     output: {
         filename: "main-[contenthash].js",
         path: path.resolve(__dirname, "build"),
         clean: true
+    },
+    module:{
+        rules :[
+            {
+                test : /\.css$/,
+                use : [ MiniCssExtractPlugin.loader ,"css-loader"]
+            }
+        ]
     }
 })
