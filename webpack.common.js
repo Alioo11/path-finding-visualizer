3294102ef0212615cd1ba46bda3f59e9a61752e1
ali <110mors110@gmail.com>
2022-04-01 13:14:19 +0430

    updated webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,43 +1,45 @@
-const path = require("path")
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
 
-const options ={}
+const options = {};
 
 module.exports = {
-    entry : "./src/index.js",
-    mode :"development",
-    output:{
-        filename: "js/main-[contenthash].js",
-        path : path.resolve(__dirname , "dist"),
-        clean : true
-    },
-    module:{
-        rules :[
-            {
-                test : /\.css$/,
-                use : ["style-loader" , "css-loader"]
-            },
-            {
-                test : /\.html$/,
-                use :['html-loader']
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: `img/[ext]/[name]_[hash][ext]`
-                }
-            },
-            {
-                test: /manifest.json$/,
-                generator: {
-                    filename: `manifest.json`
-                }
-            }
-        ]
-    },
-    plugins : [new HtmlWebpackPlugin({
-        template:"./index.html"
-    })]
-}
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "js/main-[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        type: "asset/resource",
+        generator: {
+          filename: `img/[name][ext]`,
+        },
+      },
+      {
+        test: /manifest.json$/,
+        generator: {
+          filename: `manifest.json`,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
